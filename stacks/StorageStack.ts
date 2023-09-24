b9a3f9efee621baefad3c2c07c3550d60a11484f
May Kuo <meifutoolate@gmail.com>
2023-09-24 22:38:52 +0800

    Adding a storage stack

---
+++ stacks/StorageStack.ts
@@ -0,0 +1,14 @@
+import { StackContext, Table } from 'sst/constructs';
+
+export function StorageStack({ stack }: StackContext) {
+    const table = new Table(stack, "Notes", {
+        fields: {
+            userId: 'string',
+            noteId: 'string',
+        },
+        primaryIndex: { partitionKey: 'userId', sortKey: 'noteId' },
+    })
+    return {
+        table,
+    }
+}
