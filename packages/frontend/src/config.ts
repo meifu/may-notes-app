8ba0dc7c89f159002442e78d4c3ca2c68a015163
May Kuo <meifutoolate@gmail.com>
2023-11-04 22:16:40 +0800

    Setting up our React app

---
+++ packages/frontend/src/config.ts
@@ -0,0 +1,19 @@
+const config = {
+    // Backend config
+    s3: {
+        REGION: import.meta.env.VITE_REGION,
+        BUCKET: import.meta.env.VITE_BUCKET,
+    },
+    apiGateway: {
+        RREGION: import.meta.env.VITE_REGION,
+        URL: import.meta.env.VITE_API_URL,
+    },
+    cognito: {
+        RREGION: import.meta.env.VITE_REGION,
+        USER_POOL_ID: import.meta.env.VITE_USER_POOL_ID,
+        APP_CLIENT_ID: import.meta.env.VITE_USER_POOL_CLIENT_ID,
+        IDENTITY_POOL_ID: import.meta.env.VITE_IDENTITY_POOL_ID,
+    },
+};
+
+export default config
